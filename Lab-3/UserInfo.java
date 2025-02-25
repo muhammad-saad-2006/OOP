2283b2d8ea18e54ef1d0627df828b626a07d59fc
muhammad-saad-2006 <saad2006isl@gmail.com>
2025-02-25 17:39:47 +0500

    Create UserInfo.java

---
+++ Lab-3/UserInfo.java
@@ -0,0 +1,11 @@
+class UserInfo{
+	private String userName = "MyAdmin";
+	private String userPassword = "12345";
+	
+	public String getUserName(){
+		return userName;
+	}
+	public String getUserPassword(){
+		return userPassword;
+	}
+}
